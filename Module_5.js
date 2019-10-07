b269ae6e6b752b0ff741f57af5ea734351f42c7a
RoyceSK <47389441+RoyceSK@users.noreply.github.com>
2019-10-07 19:21:19 +0300

    Update Module_5.js

--- Module_5.js
+++ Module_5.js
@@ -40,3 +40,41 @@
 //   const poly = new User({ name: 'Poly', age: 3, followers: 17 });
 //   poly.getFullInfo(); // User Poly is 3 years old and has 17 followers
 
+
+
+
+
+// class Storage {
+//   constructor(items){
+//     this.items = items;
+//   }
+//   getItems(){
+//     return this
+//   }
+//   addItem(item){
+//     storage.items.push(item)
+//   } 
+//   removeItem(item){
+//     storage.items.splice(1,1 ) 
+    
+// }}
+
+
+// const storage = new Storage([
+//   'Нанитоиды',
+//   'Пролонгер',
+//   'Железные жупи',
+//   'Антигравитатор',
+// ]);
+
+// const items = storage.getItems();
+// console.log(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+// storage.addItem('Дроид');
+// console.log(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+// storage.removeItem('Пролонгер');
+// console.log(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+
+
