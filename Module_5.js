76a4c1bc8ad50455639c8e3daac1061dd2af672d
RoyceSK <47389441+RoyceSK@users.noreply.github.com>
2019-10-07 16:15:47 +0300

    Update Module_5.js

--- Module_5.js
+++ Module_5.js
@@ -14,3 +14,29 @@
 
 // const poly = new Account('Poly', 'poly@mail.com');
 // poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+
+
+
+
+//-------------------------- Модуль 5/Задаие №2√-------------------------- 
+
+// class User {
+//   constructor({name, age, followers}) {
+//     this.name = name;
+//     this.age = age;
+//     this.followers = followers;
+// }
+// getFullInfo(){ 
+//  console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers.`);
+//   }
+// }
+ 
+
+  
+//   const mango = new User({ name: 'Mango', age: 2, followers: 20 });
+//   mango.getFullInfo(); // User Mango is 2 years old and has 20 followers
+  
+//   const poly = new User({ name: 'Poly', age: 3, followers: 17 });
+//   poly.getFullInfo(); // User Poly is 3 years old and has 17 followers
+
