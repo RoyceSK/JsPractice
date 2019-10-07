2364f505d45f886469c0975207003360f2854285
RoyceSK <47389441+RoyceSK@users.noreply.github.com>
2019-10-07 13:14:48 +0300

    Create modulePart2.js

---
+++ modulePart2.js
@@ -0,0 +1,16 @@
+//-------------------------- Модуль 5/Задаие №1√-------------------------- 
+// const Account = function(login, email){
+//   this.login = login
+//   this.email = email
+// };
+
+// Account.prototype.getInfo = function(){
+//   console.log(`name: ${this.login}, room: ${this.email}`)
+// };
+
+// console.log(Account.prototype.getInfo);
+// const mango = new Account('Mangozedog', 'mango@dog.woof');
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account('Poly', 'poly@mail.com');
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
